b32356a6cc540734e8883d605cb339dd20dc21db
Yuncong Zhang <njitzyc@gmail.com>
2025-03-09 23:58:20 -0700

    Update start script

--- src/app/main.py
+++ src/app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+import uvicorn
+
 from .models import Item
 
 app = FastAPI()
@@ -10,3 +12,7 @@ async def read_root():
 @app.post("/items/")
 async def create_item(item: Item):
     return {"item_id": item.id, "item_name": item.name}
+
+def run():
+    """Entry point for starting the application."""
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
